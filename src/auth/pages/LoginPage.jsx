d4aad43e3ab63167c1116da4af6ed541c733d0aa
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-02-18 10:54:01 -0400

    Se agrego simulacion de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+
 export const LoginPage = () => {
+
+    const navigate = useNavigate()
+
+    const onLogin = () => {
+        navigate('/', {
+            replace: true
+        })
+    }
+
     return (
-        <div>LoginPge</div>
+        <div className="container mt-5">
+            <h1>LoginPge</h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={ onLogin }
+            >
+                Login
+            </button>
+            </div>
     )
 }
